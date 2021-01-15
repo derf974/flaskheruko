a1fbc87f81aa94dcfd32f52486e54442e3e21be2
d3rf <derf@derf.fr>
2021-01-15 23:20:23 +0100

    Add vuejs + axios

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello'
+    return render_template('index.html')
+
+@app.route('/message')
+def message():
+    return 'Hello World !'
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
